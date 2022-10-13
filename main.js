6f3543f057b42f637ad914a0f43f725f6fd150ac
sabibur <sabiburrahaman720@gmail.com>
2022-10-13 21:40:17 +0530

    commited all and linked player1 vs player 2

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const linkElm = document.querySelectorAll("a");
 console.log(linkElm);
 const linksElm = linkElm[0];
 linksElm.style.color = "yellow";
-linksElm.textContent = "Google.com";
-linksElm.innerHTML = "<span>Google.com</span>";
-linksElm.setAttribute("href", "https://google.com");
+linksElm.textContent = "Lets's Play a Luck Game";
+linksElm.innerHTML = "<span>Lets's Play a Luck Game</span>";
+linksElm.setAttribute(
+  "href",
+  "https://sabibur201-rahaman.github.io/player1vsplayer2Game/"
+);
 linksElm.style.textDecoration = "none";
